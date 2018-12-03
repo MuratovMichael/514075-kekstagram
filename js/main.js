a57624b0a26dbeaa2d1eb7e5a98fd68c80f0629f
Mikhail <mikekrd@yandex.ru>
2018-12-03 22:10:09 +0300

    Поправил ошибки eslint

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 'use strict';
 
-var comments = [
+var commentsUsers = [
   {
-    avatar: "img/avatar-1.svg",
+    avatar: 'img/avatar-1.svg',
     message: 'Всё отлично!',
-    name: "Артем"
+    name: 'Артем'
   },
   {
-    avatar: "img/avatar-2.svg",
-    message: "В целом всё неплохо. Но не всё.",
-    name: "Сергей"
+    avatar: 'img/avatar-2.svg',
+    message: 'В целом всё неплохо. Но не всё.',
+    name: 'Сергей'
   },
   {
-    avatar: "img/avatar-3.svg",
+    avatar: 'img/avatar-3.svg',
     message: 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    name: "Дима"
+    name: 'Дима'
   },
   {
-    avatar: "img/avatar-4.svg",
+    avatar: 'mg/avatar-4.svg',
     message: 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    name: "Анна"
+    name: 'Анна'
   },
   {
-    avatar: "img/avatar-5.svg",
+    avatar: 'img/avatar-5.svg',
     message: 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    name: "Денис"
+    name: 'Денис'
   },
   {
-    avatar: "img/avatar-6.svg",
+    avatar: 'mg/avatar-6.svg',
     message: 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-    name: "Михаил"
+    name: 'Михаил'
   }
 ];
 
@@ -50,22 +50,22 @@ function Picture(url, likes, comments, description) {
 }
 
 var getRandomInRange = function (min, max) {
-  return Math.floor(Math.random () * (max - min) - 1);
+  return Math.floor(Math.random() * (max - min) - 1);
 };
 
 var getRandomElement = function (arr) {
-  return arr[Math.floor(Math.random () * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
 var makePicturesArray = function (number) {
   var localPictures = [];
   for (var i = 1; i <= number; i++) {
-    localPictures.push(new Picture('photos/' + i + '.jpg', getRandomInRange(15, 200), getRandomElement(comments), getRandomElement(descriptions)));
+    localPictures.push(new Picture('photos/' + i + '.jpg', getRandomInRange(15, 200), getRandomElement(commentsUsers), getRandomElement(descriptions)));
   }
   return localPictures;
 };
 
-var pictures = makePicturesArray(26);
+var pictures = makePicturesArray(25);
 
 var renderPicture = function (image) {
   var pictureTemplate = document.querySelector('#picture').content;
@@ -76,11 +76,11 @@ var renderPicture = function (image) {
   return pictureElement;
 };
 
-var renderPictures = function (pictures) {
+var renderPictures = function (picture) {
   var picturesElement = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
-  pictures.forEach(function (picture) {
-    fragment.appendChild(renderPicture(picture));
+  picture.forEach(function (elem) {
+    fragment.appendChild(renderPicture(elem));
   });
   picturesElement.appendChild(fragment);
 };
@@ -97,26 +97,22 @@ var showFillElement = function (element) {
   return socialElement;
 };
 
-var renderComments = function (pictures) {
-  var socialComments = querySelector('.social__comments'); // Блок куда будут вставляться фотографии
-  var socialCommentItem = querySelector('.social__comment');
+var renderComments = function (element) {
+  var socialComments = document.querySelector('.social__comments'); // Блок куда будут вставляться фотографии
+  var socialCommentItem = document.querySelector('.social__comment').content;
   var elementComment = socialCommentItem.cloneNode(true);
-  pictures.forEach (function (item) {
-    socialCommentItem.appendChild(showFillElement(item));
-  });
-  elementComment.querySelector('.social__picture').src='img/avatar-' + getRandomInRange(1, 6) + '.svg';
+  socialCommentItem.appendChild(showFillElement(pictures[0]));
+  elementComment.querySelector('.social__picture').src = 'img/avatar-' + getRandomInRange(1, 6) + '.svg';
   elementComment.querySelector('.social__text').textContent = element.comments;
   socialComments.appendChild(socialCommentItem);
 };
 
 renderComments(pictures);
 
-var hideCount = function () {
-  var socialCommentsCount = document.querySelector('.social__comment-count');
-  socialCommentsCount.classlist.remove('social__comment-count');
-  socialCommentsCount.classList.add('visually-hidden');
+var socialCommentsCount = document.querySelector('.social__comment-count');
+socialCommentsCount.classlist.remove('social__comment-count');
+socialCommentsCount.classList.add('visually-hidden');
 
-  var commentsLoader = document.querySelector('.comments-loader');
-  commentsLoader.classList.remove('comments-loader');
-  commentsLoader.classlist.add('visually-hidden');
-};
+var commentsLoader = document.querySelector('.comments-loader');
+commentsLoader.classList.remove('comments-loader');
+commentsLoader.classlist.add('visually-hidden');
